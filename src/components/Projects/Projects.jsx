6d5e245e1ae3fe059039e7884254512b52c35b19
EricksonRT <erik_lauri@hotmail.com>
2022-12-09 22:22:19 -0300

    visualizacion de datos en card

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,22 +1,28 @@
-import { useEffect, useState } from "react";
-import { Get_information } from "../Get_information/Get_information";
-import Loading from "../Loading/Loading";
-import WorkItemListContainer from "../WorkItemListContainer/WorkItemListContainer";
+import { useEffect, useState } from 'react';
+import { Get_information } from '../Get_information/Get_information';
+import Loading from '../Loading/Loading';
+import WorkItemListContainer from '../WorkItemListContainer/WorkItemListContainer';
 
 const Projects = () => {
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
   useEffect(() => {
     Get_information()
-      .then((resp) => console.log(resp))
+      .then((resp) => setData(resp))
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
   }, []);
   return (
     <>
-      <div className="flex">
-        <h1>Mis proyectos </h1>,
+      <div className="flex text-white">
+        <h1>Mis proyectos </h1>
+        <br />,
       </div>
-      {loading ? <Loading /> : <WorkItemListContainer />}
+      {loading ? (
+        <Loading />
+      ) : (
+        <WorkItemListContainer key={data.id} data={data} />
+      )}
     </>
   );
 };
--- src/components/WorkItemListContainer/WorkItemListContainer.jsx
+++ src/components/WorkItemListContainer/WorkItemListContainer.jsx
@@ -1,4 +1,19 @@
-const WorkItemListContainer = () => {
-  return <h1>ItemlistCaontainer</h1>;
+const WorkItemListContainer = (props) => {
+  const { data } = props;
+  // let technologies = '';
+  console.log(data);
+  return data.map((data) => (
+    <div className="container" key={data.id}>
+      <div className="">
+        <h1>{data.work_title}</h1>
+        <h2>{data.work_description}</h2>
+        {/* {data.work_technologies.forEach((technologie) =>
+          technologies.push(technologie)
+        )} */}
+        {/* <h5>{technologies}</h5> */}
+        <img src={data.work_image} alt={data.work_title} />
+      </div>
+    </div>
+  ));
 };
 export default WorkItemListContainer;
